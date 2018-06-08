3117d9f74a5cdd1adb2ad15319681f2e0e57ac22
fighter201 <lusche682@gmail.com>
2018-06-08 22:13:42 +0200

    SChau mal

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,10 +1,17 @@
 package Player;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Player {
 	
 	private int highscore, meterRun, timePlayed, avatar, xp, hp, life, speed, size, star, death, coin;
 	private float effect, level; 							//Effect: Ganze Zahl Art des Effekts, Komma Stärke | level: Level&Checkpoint 
 	private String name;
+	private static final String RES_LOC = "res/models/";
 	
 	public Player() {
 		highscore = 0;
@@ -73,6 +80,48 @@ public class Player {
 	public int changeCoin(int coin, int change) {
 		coin =+ change;
 		return coin;
+	}
+	
+	public void saveData(String profil) {
+		FileReader isr = null;
+		//File objFile = new File(RES_LOC + profil + ".txt");
+	}
+	
+	public void getData(Player player, String profil) {
+		FileReader isr = null;
+		File Data = new File(RES_LOC + profil + ".txt");
+		try {
+			isr = new FileReader(Data);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		BufferedReader reader = new BufferedReader(isr);
+		String line;
+		try {
+			while(true) {
+				line = reader.readLine();
+				if(line.startsWith("hp")) {
+					String[] currentLine = line.split(" ");
+					String b = currentLine[1];
+					int a = Integer.parseInt(b);
+					player.setHp(a);
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
 	}
 	
 	public static void main(String[] args) {
