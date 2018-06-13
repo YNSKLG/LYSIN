3a7c7cf9d3d042c76ffd73cdd5540385fdfc6994
YNSKLG <yanniskluegl1@gmail.com>
2018-06-13 21:20:15 +0200

    Joystick

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,35 +1,30 @@
 package entities;
 
 import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 import models.TexturedModel;
-/*import net.java.games.input.Component;
+import net.java.games.input.Component;
 import net.java.games.input.Controller;
-import net.java.games.input.ControllerEnvironment;*/
+import net.java.games.input.ControllerEnvironment;
 import renderEngine.DisplayManager;
-//import toolbox.InputHandler;
+import toolbox.InputHandler;
 
 public class Player extends Entity {
 	
-	private static final float RUN_SPEED = 10;
-	private static final float SPRINT_SPEED = 25;
-	private static final float GRAVITY = -15;
-	private static final float JUMP_POWER = 5;
+	private static final float WALK_SPEED = 0.05f;
+	private static final float GRAVITY = -9.80665f;
+	private static final float JUMP_POWER = 4;
 
 	private float currentSpeed = 0;
 	private float currentTurnSpeed = 0;
 	private float upwardsSpeed = 0;
 	
 	private boolean isInAir = false;
-	//private boolean sprinting = false;
 	
 	public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ,
 			float scale) {
 		super(model, position, rotX, rotY, rotZ, scale);
-		
-		
 	}
 	
 	public void move() {
@@ -62,27 +57,19 @@ public class Player extends Entity {
 	private void checkInputs() {
 				
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			increasePosition(0.05f, 0, 0);
+			increasePosition(WALK_SPEED, 0, 0);
 		} else if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			increasePosition(-0.05f, 0, 0);
+			increasePosition(-WALK_SPEED, 0, 0);
 		}
-
-		/*if(!Mouse.isButtonDown(1)) {
-			this.currentTurnSpeed = -(Mouse.getDX() * 25);
-		} else {
-			this.currentTurnSpeed = 0;
-		}*/
 		
 		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)||Keyboard.isKeyDown(Keyboard.KEY_W)) {
 			jump();
-		/*	
-			if(etwas unter dem Player) {
+			if(this.getPosition().y==0) {
 				isInAir = false;
-			}*/
+			}
 		}
 		
-		
-		/*Controller joystick = null;
+		Controller joystick = null;
 		
 		for(Controller c : ControllerEnvironment.getDefaultEnvironment().getControllers()) {
 			if(c.getType() == Controller.Type.STICK) {
@@ -94,44 +81,25 @@ public class Player extends Entity {
 		
 		for(Component c : joystick.getComponents()) {
 			
-			if(c.getName().equals("y") && !sprinting) {
-				currentSpeed = -(c.getPollData()*10);
-			} else if(c.getName().equals("x")) {
-				this.currentTurnSpeed = -(c.getPollData()*75);
-			} else if(c.getName().equals("Top")) {
-				if(c.getPollData() == 1.0) {
-					jump();
-				}
-			} else if(c.getName().equals("Thumb 2")) {
-				if(c.getPollData() == 1.0) {
-					sprinting = true;
-					currentSpeed = SPRINT_SPEED;
-				} else if(c.getPollData() == 0.0) {
-					sprinting = false;
-				}
-			} else if(c.getName().equals("Base 4")) {
-				if(c.getPollData() == 1.0) {
-					InputHandler.pauseGame();
-				}
-			} else if(c.getName().equals("Base 3")) {
-				if(c.getPollData() == 1.0) {
-					DisplayManager.setFullscreen(true);
-				}
+			if(c.getName().equals("x")) {
+				currentSpeed = -(c.getPollData()*WALK_SPEED);
 			}
-			
-		}*/
-		
-		
-		/*if(Keyboard.isKeyDown(Keyboard.KEY_W) && isInAir) {
-			this.currentSpeed = RUN_SPEED;
-			this.currentTurnSpeed = 0;
-			if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
-				this.currentSpeed = SPRINT_SPEED;
-			}
-		} else if(Keyboard.isKeyDown(Keyboard.KEY_S) && isInAir) {
-			this.currentSpeed = 0;
-			this.currentTurnSpeed = 0;
-		}*/
-	}
 
+			if(c.getName().equals("Top") && c.getPollData() == 1.0) {
+				jump();
+			} 
+
+			if(c.getName().equals("Base 4") && c.getPollData() == 1.0) {
+				InputHandler.pauseGame();
+			}
+
+			if(c.getName().equals("Base 3") && c.getPollData() == 1.0) {
+				if(!DisplayManager.isFullscreen()) {
+					DisplayManager.setFullscreen(true);
+				} else {
+					DisplayManager.setFullscreen(false);
+				}
+			}
+		}
+	}
 }
