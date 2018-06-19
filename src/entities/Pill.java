34f94a474be76ab4aaa763c99795068b9cc90133
kurtse <kurtse@r155pc15.bz-pfinztal1.dynalias.org>
2018-06-19 11:32:25 +0100

    Pillen

---
+++ src/entities/Pill.java
@@ -0,0 +1,15 @@
+package entities;
+
+import org.lwjgl.util.vector.Vector3f;
+
+public class Pill {
+	
+	private String color;
+	private Vector3f position;
+	
+	public Pill(String color, Vector3f position) {
+		this.color = color;
+		this.position = position;
+	}
+}	
+
